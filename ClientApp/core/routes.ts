0aee82a86db6fe1838b56463e992bf8f303e31d3
Yoharlin <yoharlin14@gmail.com>
2025-06-02 12:26:26 -0400

    Rutas arregladas

--- ClientApp/core/routes.ts
+++ ClientApp/core/routes.ts
@@ -1,6 +1,6 @@
 export const LANDING_ROUTE = "/";
-export const login_ROUTE = "login";
-export const DASHBOARD_ROUTE = "dashboard";
+export const login_ROUTE = "/login";
+export const DASHBOARD_ROUTE = "/dashboard";
 export const ANIMALES_ROUTE = "animales";
 export const ESPECIES_ROUTE = "especies";
 export const EJEMPLARES_ROUTE = "ejemplares";
@@ -9,7 +9,7 @@ export const TRATAMIENTOS_ROUTE = "tratamientos";
 export const NECROPSIAS_ROUTE = "necropsias";
 export const SALUD_ROUTE = "salud";
 export const USUARIOS_ROUTE = "usuarios"
-export const SIGNUP_ROUTE = "signup";
+export const SIGNUP_ROUTE = "/signup";
 export const Verify_Route = "verificar";
 export const DETAILS_ROUTE = "animales/:id";
 export const INVENTARIO_ROUTE = "inventario";
